b1fff53ed3ef4d27e8c1b0559be6e60b46048584
edenbenbow <edenbenbow@users.noreply.github.com>
2020-02-25 18:02:06 -0800

    3.7 Angular files

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { QuestionFormComponent } from './question-form/question-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuestionFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
